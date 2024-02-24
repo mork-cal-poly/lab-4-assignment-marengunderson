e7d8940362e1af82e3175e132102ac93ea73e08c
Maren Gunderson <megunder@calpoly.edu>
2024-02-23 16:13:43 -0800

    Added Fade Animation for Baby Rhino
    In order to optimize ease of reading variables, a separate baby
    rhino was created in order to hardcode in the fade variable/mechanism.
    A separate function was also created to call in the fade animation when
    adult rhino's noses hit at the right point.

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,10 @@
 let leftRhinoX = 0;
 let rightRhinoX = 0;
 let clicked = false;
+var fade;
+var fadeAmount = 0;
+
+
 function setup() {
   // These lines are fitting our canvas
   // where we want in the DOM
@@ -8,18 +12,23 @@ function setup() {
   // easier
   let myCanvas = createCanvas(750, 600);
   myCanvas.parent("canvas-parent");
+  fade = 0;
+
 }
 
 function draw() {
   background(220);
   drawBackground();
 
+  // x, y, sx, sy, headX
   drawRhino(190, 400, -0.5, 0.5, leftRhinoX);
   drawRhino(545, 400, 0.5, 0.5, rightRhinoX);
-  drawRhino(350, 500, -0.25, 0.25, 0);
+  drawBabyRhino(350, 500, -0.25, 0.25, 0);
 
   kissAnimationLeft();
   kissAnimationRight();
+  babyRhinoFade();
+
   
  // leftRhinoX = leftRhinoX - 1;
 }
@@ -51,8 +60,21 @@ if (clicked) {
   
 }
 
+function babyRhinoFade() {
   
-  function drawRhino(x, y, sx, sy, headX) {
+  if (leftRhinoX <= -10) {
+   
+ if (fade <= 0) fadeAmount= fadeAmount + 4; 
+
+if (fade>255) fadeAmount; 
+
+fade += fadeAmount; 
+   print(fade)
+    
+ }
+}
+  
+function drawRhino(x, y, sx, sy, headX) {
   
     push();
   
@@ -141,6 +163,98 @@ if (clicked) {
     pop();
     
     pop();
+    
+  }
+
+function drawBabyRhino(x, y, sx, sy, headX) {
+  
+  push();
+  
+    translate(x, y);
+    scale(sx, sy);
+    noStroke();
+    
+  
+  
+   //---------main body--------
+    fill(68,95,156, fade);
+    rect(-150, -300, 350, 200);
+    
+    //back hindquarters
+    //top triangle
+    fill(46,65,102, fade);
+    triangle(225, -325, 100, -225, 300, -225);
+    
+    //bottom triangle
+    fill(76,109,175, fade);
+    triangle(100, -225, 225, -50, 300, -225);
+    
+    //back leg
+    fill(115,141,192, fade);
+    rect(175, -75, 50, 75);
+    
+    //backrgb
+    angleMode(DEGREES)
+    fill(68,95,150, fade);
+    arc(175, 0, 100, 75, 180, 360);
+    
+    //tail
+    fill(47,73,126, fade);
+    rect(275, -225, 50, 75);
+    fill(68,95,150, fade);
+    triangle(300, -150, 275, -100, 325, -100);
+    
+    
+    //front shoulder
+    //top triangle
+    fill(49,68,107, fade);
+    triangle(-150, -325, -200, -250, 0, -250);
+    
+    //bottom triangle
+    fill(123,150,204, fade);
+    triangle(-200, -250, -150, -100, 0, -250);
+    
+    //horizontal rectangle
+    fill(46,70,117, fade);
+    rect(-200, -125, 100, 50);
+    
+    //vertical rectangle
+    fill(107,129,175, fade);
+    rect(-200, -75, 50, 50);
+    
+    //paw
+    fill(35,56,97, fade);
+    arc(-175, -30, 100, 60, 270, 90);
+    
+    
+    //-----------head----------
+
+    //main head shape
+    fill(32,47,78, fade);
+    triangle(-200, -310, -350, -125, -145, -175);
+    
+    //nose
+    fill(83,114,177, fade);
+    rect(-350, -200, 100, 75);
+    
+    //ears
+    //left ear
+    push();
+    rotate(45);
+    arc(-390, -75, 50, 25, 180, 0);
+    pop();
+    
+    //right ear
+    fill(49,75,127, fade);
+    arc(-200, -330, 25, 50, 270, 90);
+    //eye
+    fill(300, fade);
+    ellipse(-210, -210, 15, 15);
+    
+     //horn
+    triangle(-300, -275, -350, -200, -285, -200);
+    
+  pop();
     
   }
 
